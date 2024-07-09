8d5dbab920425846efdadeae08fc878315e7321d
harubiya <hello@gilai.net>
2024-07-09 13:31:36 +0900

    ignore

--- gitinit.sh
+++ gitinit.sh
@@ -4,7 +4,7 @@ git config --global user.name "harubiya"
 
 echo "# blog" >> README.md
 git init
-git add README.md
+git add .
 git commit -m "first commit"
 git branch -M main
 git remote add origin git@github.com:harubiya/blog.git
