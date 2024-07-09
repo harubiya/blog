2d048aa7f17364079b7275a1e6a2a79e2b75e291
harubiya <hello@gilai.net>
2024-07-09 13:26:42 +0900

    first upload

---
+++ gitinit.sh
@@ -0,0 +1,14 @@
+git config --global user.email "hello@gilai.net"
+git config --global user.name "harubiya"
+
+
+echo "# blog" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin git@github.com:harubiya/blog.git
+git remote -v
+
+ssh -T git@github.com
+git push -u origin main
